140016f95554dd2bee126fe3444bcf81fbbefe6b
BreeZ27 <khavlaan@gmail.com>
2021-12-22 18:52:19 +0300

    Update code.js

---
+++ code.js
@@ -0,0 +1,11 @@
+// Код для кликабельности названий пунктов меню
+// Пункт "О нас"
+$("[href = '#submenu:about']").click(function() {document.location="/aboutus"})
+// Пункт "Образование"
+$("[href = '#submenu:study']").click(function() {document.location="/education"})
+// Пункт "Охрана труда"
+$("[href = '#submenu:safe']").click(function() {document.location="/healthsafety"})
+// Пункт "Лабораторные исследования"
+$("[href = '#submenu:lab']").click(function() {document.location="/labsresearches"})
+// Пункт "Информация"
+$("[href = '#submenu:info']").click(function() {document.location="/info"})
